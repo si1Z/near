e3ae14dfdfd563a2ad3b126b62661ad4a075efca
sim <zhujinghui1@xiaomi.com>
2020-12-15 18:00:06 +0800

    hello

---
+++ start.go
@@ -0,0 +1,19 @@
+/*
+@Time : 2020/12/15 5:53 下午
+@Author : sim
+@File : start
+@Software: GoLand
+*/
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
